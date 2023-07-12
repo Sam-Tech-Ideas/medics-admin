3e0ae792356196918601d9d5a422e550b47b1192
Sam-Tech-Ideas <agyemangsamuel235@gmail.com>
2023-07-12 15:30:17 +0000

    upload

--- src/App.tsx
+++ src/App.tsx
@@ -5,193 +5,32 @@ import { Authenticator, buildCollection, FirebaseCMSApp } from "firecms";
 
 import "typeface-rubik";
 import "@fontsource/ibm-plex-mono";
+import { categories } from "./enums";
 
 // TODO: Replace with your config
 const firebaseConfig = {
-  apiKey: "AIzaSyAoE1F2o3WBpJaBh4ikFeVaEI6aFeyu0ck",
-  authDomain: "ecclesia-church.firebaseapp.com",
-  projectId: "ecclesia-church",
-  storageBucket: "ecclesia-church.appspot.com",
-  messagingSenderId: "975793776023",
-  appId: "1:975793776023:web:8e11618d50db0eae6490d2",
-};
-// const locales = {
-//     "en-US": "English (United States)",
-//     "es-ES": "Spanish (Spain)",
-//     "de-DE": "German
-// 
-
-type Event = {
-  id: string;
-  title: string;
-
-  imageUrl: string;
-  link: string;
-  description: string;
-  videoLink: string;
-
-  date: Date;
+  apiKey: "AIzaSyAygio7gVXQoHVJujtbbmLHu6wx5zaY4VY",
+  authDomain: "medical-75ae7.firebaseapp.com",
+  projectId: "medical-75ae7",
+  storageBucket: "medical-75ae7.appspot.com",
+  messagingSenderId: "801283588951",
+  appId: "1:801283588951:web:d7bcd8a3ff129a70df3412",
 };
 
-const eventsCollection = buildCollection<Event>({
-  name: "Events",
-  singularName: "Event",
-  path: "events",
-  permissions: ({}) => ({
-    read: true,
-    edit: true,
-    create: true,
-    delete: true,
-  }),
-
-  properties: {
-    id: {
-      name: "ID",
-      dataType: "string",
-      validation: { required: true },
-    },
-
-    title: {
-      name: "Title",
-      dataType: "string",
-      validation: { required: true },
-    },
-
-    imageUrl: {
-      name: "Image",
-      dataType: "string",
-      storage: {
-        storagePath: "images",
-        acceptedFiles: ["image/*"],
-      },
-    },
-    link: {
-      name: "Link",
-      dataType: "string",
-    },
-    description: {
-      name: "Description",
-      dataType: "string",
-    },
-    videoLink: {
-      name: "Video Link",
-      dataType: "string",
-    },
-    date: {
-      name: "Date",
-      dataType: "date",
-      validation: { required: true },
-    },
-  },
-});
-
-type Users = {
+type User = {
   id: string;
-  username: string;
+  name: string;
   email: string;
-  date: Date;
-  fullName: string;
+  role: string;
   photo: string;
-  profileType: string;
   phoneNumber: string;
 };
 
-const usersCollection = buildCollection<Users>({
+const usersCollection = buildCollection<User>({
   name: "Users",
   singularName: "User",
   path: "users",
-  permissions: ({  }) => ({
-    read: true,
-    edit: true,
-    create: true,
-    delete: true,
-  }),
-
-  properties: {
-    id: {
-      name: "ID",
-      dataType: "string",
-      validation: { required: true },
-    },
-    username: {
-      name: "Username",
-      dataType: "string",
-      validation: { required: true },
-    },
-    email: {
-      name: "Email",
-      dataType: "string",
-      validation: { required: true },
-    },
-    date: {
-      name: "Date",
-      dataType: "date",
-    },
-    fullName: {
-      name: "Full Name",
-      dataType: "string",
-    },
-    photo: {
-      name: "Photo",
-      dataType: "string",
-    },
-    profileType: {
-      name: "Profile Type",
-      dataType: "string",
-    },
-    phoneNumber: {
-      name: "Phone Number",
-      dataType: "string",
-    },
-  },
-});
-
-type Podcast = {
-  id: string;
-  link: string;
-};
-
-
-
-const podcastsCollection = buildCollection<Podcast>({
-  name: "Podcasts",
-  singularName: "Podcast",
-  path: "podcasts",
-  permissions: ({  }) => ({
-    read: true,
-    edit: true,
-    create: true,
-    delete: true,
-  }),
-
-  properties: {
-    id: {
-      name: "ID",
-      dataType: "string",
-      validation: { required: true },
-    },
-    link: {
-      name: "Link",
-      dataType: "string",
-      validation: { required: true },
-    },
-  },
-});
-
-type Giving = {
-  id: string;
-  name: string;
-  amount: number;
-  date_paid: Date;
-  payment_method: string;
-  giving_type: string;
-};
-
-const givingCollection = buildCollection<Giving>({
-  name: "Givings",
-  singularName: "Giving",
-  path: "givings",
-  permissions: ({ }) => ({
+  permissions: ({}) => ({
     read: true,
     edit: true,
     create: true,
@@ -206,33 +45,325 @@ const givingCollection = buildCollection<Giving>({
     },
     name: {
       name: "Name",
-
       dataType: "string",
       validation: { required: true },
     },
-    amount: {
-      name: "Amount",
-      dataType: "number",
-      validation: { required: true },
-    },
-    date_paid: {
-      name: "Date Paid",
-      dataType: "date",
-      validation: { required: true },
-    },
-
-    payment_method: {
-      name: "Payment Method",
+    email: {
+      name: "Email",
       dataType: "string",
       validation: { required: true },
     },
-    giving_type: {
-      name: "Giving Type",
+    role: {
+      name: "Role",
+      dataType: "string",
+      validation: { required: true },
+    },
+    photo: {
+      name: "Photo",
+      dataType: "string",
+      validation: { required: true },
+    },
+    phoneNumber: {
+      name: "Phone Number",
       dataType: "string",
       validation: { required: true },
     },
   },
 });
+
+type Product = {
+  title: string;
+  price: number;
+  location: string;
+  description: string;
+  imageUrl: string;
+  category: string;
+  quantity: number;
+  composition: string;
+  vendor: string;
+};
+
+const productsCollection = buildCollection<Product>({
+  name: "Products",
+  singularName: "Product",
+  path: "products",
+  permissions: ({}) => ({
+    read: true,
+    edit: true,
+    create: true,
+    delete: true,
+  }),
+
+  properties: {
+    // id: {
+    //   name: "ID",
+    //   dataType: "string",
+    //   validation: { required: true },
+    // },
+    title: {
+      name: "Title",
+      dataType: "string",
+      validation: { required: true },
+    },
+    price: {
+      name: "Price",
+      dataType: "number",
+      validation: { required: true },
+    },
+    description: {
+      name: "Description",
+      dataType: "string",
+      validation: { required: true },
+    },
+    imageUrl: {
+      name: "Product image",
+      dataType: "string",
+      storage: {
+        storagePath: "images",
+        acceptedFiles: ["product/*"],
+      },
+    },
+    category: {
+      name: "Category",
+      dataType: "string",
+      enumValues: categories,
+      validation: { required: true },
+      
+      //i want user to selct from a list
+      
+    },
+    quantity: {
+      name: "Quantity",
+      dataType: "number",
+      validation: { required: true },
+    },
+    composition: {
+      name: "Composition",
+      dataType: "string",
+      validation: { required: true },
+    },
+    vendor: {
+      name: "Vendor",
+      dataType: "string",
+      validation: { required: true },
+    },
+    location: {
+      name: "Location",
+      dataType: "string",
+      validation: { required: true },
+    },
+  },
+});
+
+type Order = {
+  id: string;
+  userId: string; //
+  items: string[];
+  status: string;
+  total: number;
+  payment_reference: string;
+};
+
+const ordersCollection = buildCollection<Order>({
+  name: "Orders",
+  singularName: "Order",
+  path: "orders",
+  permissions: ({}) => ({
+    read: true,
+    edit: true,
+    create: true,
+    delete: true,
+  }),
+
+  properties: {
+    id: {
+      name: "ID",
+      dataType: "string",
+      validation: { required: true },
+    },
+    userId: {
+      name: "User ID",
+      dataType: "string",
+      validation: { required: true },
+    },
+    items: {
+      name: "Items",
+
+      dataType: "array",
+      of: {
+        dataType: "reference",
+        path: "products",
+        previewProperties: ["id", "title", "price", "quantity"],
+      },
+
+      validation: { required: true },
+    },
+
+    status: {
+      name: "Status",
+      dataType: "string",
+      validation: { required: true },
+    },
+    total: {
+      name: "Total",
+      dataType: "number",
+      validation: { required: true },
+    },
+
+    payment_reference: {
+      name: "Payment Reference",
+      dataType: "string",
+      validation: { required: true },
+    },
+  },
+});
+
+// type Users = {
+//   id: string;
+//   username: string;
+//   email: string;
+//   date: Date;
+//   fullName: string;
+//   photo: string;
+//   profileType: string;
+//   phoneNumber: string;
+// };
+
+// const usersCollection = buildCollection<Users>({
+//   name: "Users",
+//   singularName: "User",
+//   path: "users",
+//   permissions: ({  }) => ({
+//     read: true,
+//     edit: true,
+//     create: true,
+//     delete: true,
+//   }),
+
+//   properties: {
+//     id: {
+//       name: "ID",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     username: {
+//       name: "Username",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     email: {
+//       name: "Email",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     date: {
+//       name: "Date",
+//       dataType: "date",
+//     },
+//     fullName: {
+//       name: "Full Name",
+//       dataType: "string",
+//     },
+//     photo: {
+//       name: "Photo",
+//       dataType: "string",
+//     },
+//     profileType: {
+//       name: "Profile Type",
+//       dataType: "string",
+//     },
+//     phoneNumber: {
+//       name: "Phone Number",
+//       dataType: "string",
+//     },
+//   },
+// });
+
+// type Podcast = {
+//   id: string;
+//   link: string;
+// };
+
+// const podcastsCollection = buildCollection<Podcast>({
+//   name: "Podcasts",
+//   singularName: "Podcast",
+//   path: "podcasts",
+//   permissions: ({  }) => ({
+//     read: true,
+//     edit: true,
+//     create: true,
+//     delete: true,
+//   }),
+
+//   properties: {
+//     id: {
+//       name: "ID",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     link: {
+//       name: "Link",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//   },
+// });
+
+// type Giving = {
+//   id: string;
+//   name: string;
+//   amount: number;
+//   date_paid: Date;
+//   payment_method: string;
+//   giving_type: string;
+// };
+
+// const givingCollection = buildCollection<Giving>({
+//   name: "Givings",
+//   singularName: "Giving",
+//   path: "givings",
+//   permissions: ({ }) => ({
+//     read: true,
+//     edit: true,
+//     create: true,
+//     delete: true,
+//   }),
+
+//   properties: {
+//     id: {
+//       name: "ID",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     name: {
+//       name: "Name",
+
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     amount: {
+//       name: "Amount",
+//       dataType: "number",
+//       validation: { required: true },
+//     },
+//     date_paid: {
+//       name: "Date Paid",
+//       dataType: "date",
+//       validation: { required: true },
+//     },
+
+//     payment_method: {
+//       name: "Payment Method",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//     giving_type: {
+//       name: "Giving Type",
+//       dataType: "string",
+//       validation: { required: true },
+//     },
+//   },
+// });
 
 export default function App() {
   const myAuthenticator: Authenticator<FirebaseUser> = useCallback(
@@ -256,12 +387,7 @@ export default function App() {
     <FirebaseCMSApp
       name={"Ecclesias Admin"}
       authentication={myAuthenticator}
-      collections={[
-        eventsCollection,
-        usersCollection,
-        podcastsCollection,
-        givingCollection,
-      ]}
+      collections={[productsCollection, usersCollection, ordersCollection]}
       firebaseConfig={firebaseConfig}
     />
   );
---
+++ src/enums.tsx
@@ -0,0 +1,10 @@
+import { EnumValues } from "firecms";
+
+export enum categories {
+  "Condoms" = "Condoms",
+  "Lubricants" = "Lubricants",
+  "Contraceptives" = "Contraceptives",
+  "Feminine Hygiene" = "Feminine Hygiene",
+  "Pregnancy Test" = "Pregnancy Test",
+  "Sexual Health" = "Sexual Health",
+}
